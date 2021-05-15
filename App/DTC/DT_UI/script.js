23ee7630dfdfd1f9f066c849a56a5dbea9408f9a
nairdevika <nairdevja18ce@student.mes.ac.in>
2021-05-15 15:03:35 +0530

    App with 3 models

---
+++ App/DTC/DT_UI/script.js
@@ -0,0 +1,39 @@
+document.getElementById('train-model').addEventListener('click', async (e) => {
+    e.preventDefault()
+    let accuracy = "The accuracy of the model is ";
+    await fetch('http://127.0.0.1:5000/trainModel', {
+        mode: 'cors', // no-cors, *cors, same-origin
+    })
+        .then(response => response.json())
+        .then(data => accuracy += data);
+
+    document.getElementsByClassName('results')[0].style.display = 'flex';
+    document.getElementById('accuracy').textContent = accuracy + "%";
+    document.getElementById('predict').disabled = false
+})
+
+document.getElementById('predict').addEventListener('click', async (e) => {
+    e.preventDefault()
+    let data = []
+    let result;
+    let inputs = Array.from(document.getElementsByClassName('input_field'))
+    inputs.map(i => data.push(parseFloat(i.value)))
+
+    await fetch('http://127.0.0.1:5000/predict', {
+        mode: 'cors', // no-cors, *cors, same-origin
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(d => result = d);
+
+    if (result === 'B') {
+        document.getElementById('result').textContent = "The tumor is Benign"
+    } else if (result === 'M') {
+        document.getElementById('result').textContent = "The tumor is Malignant"
+    }
+})
+
+document.getElementById('reset').addEventListener('click', (e) => {
+    document.getElementById('result').textContent = '';
+})
